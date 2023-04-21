3e937be4393cdc48b5fbcfd4aad1d96fdc5002e6
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-21 17:47:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+		this._width = width;
+	}
+
+	get height(){
+		this._height = height;
+	}
+
+	getArea(){
+		return this._width * this._height;
+	}
+}
+
+class Square extends Rectangle {
+	getPerimeter(){
+		return 2 * this._width * this._height;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
